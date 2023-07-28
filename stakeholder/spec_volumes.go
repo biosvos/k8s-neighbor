4893018930ac1209dc2e828e9c31e078595bf492
jinsu <realwaterkim@datacommand.kr>
2023-07-28 14:13:17 +0900

    projected volume 추가

--- stakeholder/spec_volumes.go
+++ stakeholder/spec_volumes.go
@@ -26,19 +26,35 @@ func parseSpecVolumes(parser *json.Parser, namespace string) []*domain.ResourceI
 
 	var ret []*domain.ResourceIdentifier
 	for _, node := range volumeNodes {
-		one, err := node.ParseOne(".configMap.name")
-		if err != nil {
-			continue
-		}
-		name := one.GetMustString()
-		ret = append(ret, &domain.ResourceIdentifier{
-			GVK: &domain.GroupVersionKind{
-				Version: "v1",
-				Kind:    "ConfigMap",
-			},
-			Namespace: namespace,
-			Name:      name,
-		})
+		ret = appendConfigMapName(ret, node, namespace)
+		ret = appendProjectedSources(ret, node, namespace)
 	}
 	return ret
+}
+
+func appendProjectedSources(ret []*domain.ResourceIdentifier, parser *json.Parser, namespace string) []*domain.ResourceIdentifier {
+	projectedNodes, err := parser.Parse(".projected.sources[*]")
+	if err != nil {
+		return ret
+	}
+	for _, projectedNode := range projectedNodes {
+		ret = appendConfigMapName(ret, projectedNode, namespace)
+	}
+	return ret
+}
+
+func appendConfigMapName(slice []*domain.ResourceIdentifier, parser *json.Parser, namespace string) []*domain.ResourceIdentifier {
+	one, err := parser.ParseOne(".configMap.name")
+	if err != nil {
+		return slice
+	}
+	name := one.GetMustString()
+	return append(slice, &domain.ResourceIdentifier{
+		GVK: &domain.GroupVersionKind{
+			Version: "v1",
+			Kind:    "ConfigMap",
+		},
+		Namespace: namespace,
+		Name:      name,
+	})
 }
