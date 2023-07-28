e5ba33be30a0fa17249c21b5184f9ffab23ba0f5
jinsu <realwaterkim@datacommand.kr>
2023-07-28 11:52:04 +0900

    리팩토링

--- flow/flow_test.go
+++ flow/flow_test.go
@@ -2,11 +2,13 @@ package flow
 
 import (
 	"github.com/biosvos/k8s-neighbor/client"
+	"github.com/stretchr/testify/require"
 	"testing"
 )
 
 func TestName(t *testing.T) {
 	newClient, _ := client.NewClient()
 	flow := NewFlow(newClient)
+	require.NotNil(t, flow)
 	flow.GetWorkloadResources("", "v1", "Pod", "kube-system", "coredns-59b4f5bbd5-kbrvk")
 }
--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
-	"github.com/biosvos/k8s-neighbor/internal"
+	"github.com/biosvos/k8s-neighbor/client"
+	"github.com/biosvos/k8s-neighbor/flow"
 	"log"
 )
 
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
-	err := internal.Work()
-	log.Println(err)
+	cli, err := client.NewClient()
+	if err != nil {
+		panic(err)
+	}
+	flo := flow.NewFlow(cli)
+	flo.GetWorkloadResources("", "v1", "Pod", "kube-system", "coredns-59b4f5bbd5-kbrvk")
 }
