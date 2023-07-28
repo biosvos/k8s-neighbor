7aa9b6965a02648164714c8018477af5c88957c9
jinsu <realwaterkim@datacommand.kr>
2023-07-28 10:50:50 +0900

    parser jsonpath

--- json/parser.go
+++ json/parser.go
@@ -1,8 +1,10 @@
 package json
 
 import (
+	"fmt"
 	"github.com/pkg/errors"
 	"github.com/spyzhov/ajson"
+	"strconv"
 )
 
 func NewParser(contents []byte) (*Parser, error) {
@@ -17,4 +19,51 @@ func NewParser(contents []byte) (*Parser, error) {
 
 type Parser struct {
 	root *ajson.Node
+}
+
+func (p *Parser) Parse(path string) ([]*Parser, error) {
+	result, err := p.root.JSONPath(fmt.Sprintf("@%v", path))
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	var ret []*Parser
+	for _, node := range result {
+		ret = append(ret, &Parser{root: node})
+	}
+	return ret, nil
+}
+
+func (p *Parser) ParseOne(path string) (*Parser, error) {
+	result, err := p.Parse(path)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	if len(result) != 1 {
+		return nil, errors.New("not one")
+	}
+	return result[0], nil
+}
+
+func (p *Parser) GetMustString() string {
+	ret, err := p.GetString()
+	if err != nil {
+		panic(err)
+	}
+	return ret
+}
+
+func (p *Parser) GetString() (string, error) {
+	switch p.root.Type() { //nolint:exhaustive
+	case ajson.String:
+		value, _ := p.root.GetString()
+		return value, nil
+	case ajson.Numeric:
+		value, _ := p.root.GetNumeric()
+		return strconv.FormatFloat(value, 'f', -1, 64), nil
+	case ajson.Bool:
+		value, _ := p.root.GetBool()
+		return strconv.FormatBool(value), nil
+	default:
+		return "", errors.New("wrong type")
+	}
 }
--- json/parser_test.go
+++ json/parser_test.go
@@ -23,3 +23,39 @@ func TestNewParser(t *testing.T) {
 	require.NoError(t, err)
 	require.NotNil(t, parser)
 }
+
+func TestParse(t *testing.T) {
+	parser, _ := NewParser([]byte(`{
+  "metadata": {
+    "ownerReferences": [
+      {
+        "apiVersion": "apps/v1",
+        "blockOwnerDeletion": true,
+        "controller": true,
+        "kind": "ReplicaSet",
+        "name": "coredns-59b4f5bbd5",
+        "uid": "99e999e3-9f2d-4d35-8aac-9abd9bd6928a"
+      }
+    ]
+  }
+}`))
+
+	nodes, err := parser.Parse(".metadata.ownerReferences[*]")
+
+	require.NoError(t, err)
+	require.Len(t, nodes, 1)
+
+	for _, node := range nodes {
+		kinds, _ := node.Parse(".kind")
+		getString, err := kinds[0].GetString()
+
+		require.NoError(t, err)
+		require.Equal(t, "ReplicaSet", getString)
+
+		blockOwnerDeletion, _ := node.ParseOne(".blockOwnerDeletion")
+		getString, err = blockOwnerDeletion.GetString()
+
+		require.NoError(t, err)
+		require.Equal(t, "true", getString)
+	}
+}
